e925c1ad95a049417708d8ad691a6b99d1fcf704
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-04-09 23:26:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class _MyAppState extends State<MyApp> {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Hamza',
+        title: 'Mustafa',
         theme: ThemeStyles.themeData(_themeProvider.lightTheme, context),
         initialRoute: "/",
         routes: {
